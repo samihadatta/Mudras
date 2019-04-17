18f50db2c56905ebb9003a4431b6ae9bc5b09fc9
Bo Bleckel <bbleckel@me.com>
2019-04-17 10:30:22 -0400

    test that I will remove in a second

---
+++ test/test/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  test
+//
+//  Created by bbleckel on 4/17/19.
+//  Copyright © 2019 bbleckel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
